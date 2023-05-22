4953f33d85abf5b8f913a3b639c3f41f2ebaaf4e
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-23 06:51:45 +0900

    ✨ Feat: Header 컴포넌트 생성

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Text } from "react-native";
 
 import Layout from "../components/Layout";
-import Margin from "../components/margin/Margin";
+import Header from "../components/Header";
+import Margin from "../components/Margin";
 
 const UserScreen: React.FC = () => {
   return (
     <Layout>
+      <Header text="사용자 화면"/>
       <Margin props={16} />
       <Text>사용자 화면입니당!</Text>
     </Layout>
