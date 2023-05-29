28acbee14e8a76758230306d35045347e760266f
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-29 09:37:46 +0900

    ➕ Feat: AsyncStorage 라이브러리 설치 및 OnBoardingScreen 구현 중

---
+++ src/pages/OnBoarding/SecondPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components/native";
+
+
+interface SecondPageProps {
+  setPageStatus: React.Dispatch<React.SetStateAction<string>>;
+};  
+
+const SecondPage = ({ setPageStatus }: SecondPageProps) => {
+
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default SecondPage;
---
+++ src/pages/OnBoarding/ThirdPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components/native";
+
+
+interface ThirdPageProps {
+  setPageStatus: React.Dispatch<React.SetStateAction<string>>;
+};  
+
+const ThirdPage = ({ setPageStatus }: ThirdPageProps) => {
+
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default ThirdPage;
