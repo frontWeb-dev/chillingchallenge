2263ac1be93571277b9211e368d6af27c29a1370
frontWeb-dev <101559564+frontWeb-dev@users.noreply.github.com>
2023-05-29 13:52:17 +0900

    :sparkles: Card 컴포넌트 클릭 시 미션 스크린으로 이동
    - mission 스크린에 미션 정보 데이터 넘겨줌

--- src/screens/Mission/MissionScreen.tsx
+++ src/screens/Mission/MissionScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Layout from "../../components/Layout";
 import Header from "../../components/Header";
@@ -6,38 +6,38 @@ import Margin from "../../components/Margin";
 import StartPage from "../../pages/Mission/StartPage";
 import InProgressPage from "../../pages/Mission/InProgressPage";
 import CompletePage from "../../pages/Mission/CompletePage";
+import { RouteProp, useRoute } from "@react-navigation/native";
+import { MissionData } from "./SelectScreen";
+
+type RootStackParamList = {
+  MissionScreen: { data: MissionData };
+};
 
 const MissionScreen: React.FC = () => {
+  const { params } = useRoute<RouteProp<RootStackParamList, "MissionScreen">>();
+
   const [missionStatus, setMissionStatus] = useState("Start");
 
   let pageComponent;
 
   switch (missionStatus) {
     case "Start":
-      pageComponent = <StartPage 
-        setMissionStatus={setMissionStatus}
-        
-      />;
+      pageComponent = <StartPage setMissionStatus={setMissionStatus} />;
       break;
     case "InProgress":
-      pageComponent = <InProgressPage
-        setMissionStatus={setMissionStatus}
-      />;
+      pageComponent = <InProgressPage setMissionStatus={setMissionStatus} />;
       break;
     case "Complete":
-      pageComponent = <CompletePage
-        setMissionStatus={setMissionStatus}
-      />;
+      pageComponent = <CompletePage setMissionStatus={setMissionStatus} />;
       break;
     default:
       pageComponent = null;
   }
 
-
   return (
     <Layout>
-      <Header text="Mission A"/>
-      <Margin props={30}/>
+      <Header text={params?.data?.title} />
+      <Margin props={30} />
       {pageComponent}
     </Layout>
   );
--- src/screens/Mission/SelectScreen.tsx
+++ src/screens/Mission/SelectScreen.tsx
@@ -4,14 +4,36 @@ import Header from "../../components/Header";
 import { missions } from "../../mocks/missions";
 import Card from "../../components/Card";
 import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
+import { MissionNavigatorParamList } from "../../navigations/MissionNavigator";
+
+export interface MissionData {
+  id: number;
+  title: string;
+  comment: string;
+  desc: string;
+  bedge?: string;
+  bgImage: string;
+}
 
 const SelectScreen: React.FC = () => {
+  const navigation = useNavigation<MissionNavigatorParamList>();
+  const onPress = (el: MissionData) => {
+    if (el.bedge === "미션 완료") return;
+    navigation.navigate("MissionScreen", { data: el });
+  };
+
   return (
     <Layout>
       <Header text="오늘의 칠링챌링" />
       <Container>
         {missions.map((el) => (
-          <Card key={el.id} isDone={el.bedge === "미션 완료"} bedge={el.bedge}>
+          <Card
+            key={el.id}
+            isDone={el.bedge === "미션 완료"}
+            bedge={el.bedge}
+            onPress={() => onPress(el)}
+          >
             <ImageBackground
               source={{ uri: el.bgImage }}
               resizeMode="cover"
