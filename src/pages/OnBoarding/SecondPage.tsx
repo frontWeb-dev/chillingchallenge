df89a7ed8dca0efe22e910f8dda102caad91113b
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-29 11:04:52 +0900

    ✨ OnBoardingFooter 컴포넌트 UI 구현

--- src/pages/OnBoarding/SecondPage.tsx
+++ src/pages/OnBoarding/SecondPage.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components/native";
 
 import MediaContainer from "../../components/onBoarding/MediaContainer";
 import Margin from "../../components/Margin";
+import DescriptionContainer from "../../components/onBoarding/DescriptionContainer";
+import OnBoardingFooter from "../../components/onBoarding/OnBoardingFooter";
 
 interface SecondPageProps {
   setPageStatus: React.Dispatch<React.SetStateAction<string>>;
@@ -15,6 +17,18 @@ const SecondPage = ({ setPageStatus }: SecondPageProps) => {
     <>
       <MediaContainer
         props={1}
+      />
+      <Margin props={50} />
+      <DescriptionContainer
+        headerText="프론트 잘하면 좋겠당"
+        contentText={`함께 미션을 하는 사람들과
+감정을 공유하고
+응원을 주고 받아요.`
+        }
+      />
+      <OnBoardingFooter
+        step={2}
+        setPageStatus={setPageStatus}
       />
     </>
   );
