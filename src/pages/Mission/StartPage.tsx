cca315523cc0b63368112ddbd3acf12fb71fe823
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-30 15:00:27 +0900

    💄 Style: StartPage 컴포넌트 UI 디자인

--- src/pages/Mission/StartPage.tsx
+++ src/pages/Mission/StartPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components/native";
 
 import LongButton from "../../components/mission/LongButton";
+import Margin from "../../components/Margin";
 
 interface StartPageProps {
   setMissionStatus: React.Dispatch<React.SetStateAction<string>>;
@@ -13,15 +14,30 @@ interface StartPageProps {
   type: number;
 };  
 
-const StartPage = ({ setMissionStatus }: StartPageProps) => {
-
+const StartPage = ({ setMissionStatus, comment, desc }: StartPageProps) => {
 
   return (
     <>
       <Wrapper>
         <MissionQuote>
-          미션 시작
+          "{comment}"
         </MissionQuote>
+        <Margin props={50} />
+        <ContentContainer>
+          <SubHeader>
+            챌린지 설명
+          </SubHeader>
+          <Description>
+            {desc}
+          </Description>
+          <Margin props={10} />
+          <SubHeader>
+            챌린지 방법
+          </SubHeader>
+          <Description>
+            {desc}
+          </Description>
+        </ContentContainer>
         <LongButton
           type="START"
           text="미션 시작하기"
@@ -40,13 +56,37 @@ const Wrapper = styled.View`
   width: 100%;
   flex: 1;
   display: flex;
-  justify-content: space-between;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   padding: 10px 20px;
 `;
 
 const MissionQuote = styled.Text`
-  font-size: 40px;
-  font-family: "ExtraBold";
+  font-size: 18px;
+  font-family: "Light";
+  font-style: italic;
+  color: grey;
+`;
+
+const ContentContainer = styled.View`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 15px;
+`;
+
+const SubHeader = styled.Text`
+  font-size: 16px;
+  font-family: "Medium";
+`;
+
+const Description = styled.Text`
+  font-size: 14px;
+  font-family: "Regular";
+  color: dimgrey;
+  line-height: 24px;
+  text-align: center;
 `;
