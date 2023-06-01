95bb92b157bca9bdf7eb5b86b18567050df444d2
frontWeb-dev <101559564+frontWeb-dev@users.noreply.github.com>
2023-06-01 13:37:29 +0900

    :bug: TextInput 텍스트 안보이는 이슈 수정

--- src/components/happy/HappyText.tsx
+++ src/components/happy/HappyText.tsx
@@ -4,9 +4,9 @@ import styled from "styled-components/native";
 interface HappyTextProps {
   happyText: string;
   setHappyText: React.Dispatch<React.SetStateAction<string>>;
-};
+}
 
-const HappyText = ({happyText, setHappyText} : HappyTextProps) => {
+const HappyText = ({ happyText, setHappyText }: HappyTextProps) => {
   const onChangeText = (text: string) => {
     setHappyText(text);
   };
@@ -15,13 +15,16 @@ const HappyText = ({happyText, setHappyText} : HappyTextProps) => {
     <>
       <Wrapper>
         <HappyDesc>
-          이번 활동을 하면서 느낀 점을 짧게 쓰고,{"\n"}방금 한 챌린지가 행복감을 키우는 데에{"\n"}얼마나 도움이 되었는지 알려주세요.
+          이번 활동을 하면서 느낀 점을 짧게 쓰고,{"\n"}방금 한 챌린지가 행복감을 키우는 데에{"\n"}
+          얼마나 도움이 되었는지 알려주세요.
         </HappyDesc>
-        <HappyTextInput
-          placeholder="텍스트를 입력하세요."
-          onChangeText={(text: string) => onChangeText(text)}
-          value={happyText}
-        />
+        <HappyForm>
+          <HappyTextInput
+            placeholder="텍스트를 입력하세요."
+            onChangeText={(text: string) => onChangeText(text)}
+            value={happyText}
+          />
+        </HappyForm>
       </Wrapper>
     </>
   );
@@ -46,14 +49,11 @@ const HappyDesc = styled.Text`
   line-height: 28px;
 `;
 
-const HappyTextInput = styled.TextInput`
+const HappyForm = styled.View`
   width: 100%;
-  padding: 20px;
-  margin-top: 30px;
-  margin-bottom: 20px;
-  flex: 1;
-  justify-content: space-between;
   border: 1px solid #ccc;
+  padding: 20px;
   border-radius: 20px;
-  align-self: stretch;
 `;
+
+const HappyTextInput = styled.TextInput``;
