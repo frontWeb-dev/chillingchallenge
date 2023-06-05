d721b8a554ec269644e80732d40881147d851edf
frontWeb-dev <101559564+frontWeb-dev@users.noreply.github.com>
2023-06-05 11:22:45 +0900

    :sparkles: Begde 컴포넌트 구현 / mock 데이터 생성

--- src/components/profile/Bedge.tsx
+++ src/components/profile/Bedge.tsx
@@ -1,12 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
+import bedges from "../../mocks/bedges";
 
-const Bedge = () => {
-  return <Container></Container>;
+const UserBedgeScreen = () => {
+  const renderCell = (index: number) => {
+    return (
+      <Bedge>
+        <BedgeImage
+          source={bedges[index].type === "active" ? bedges[index].active : bedges[index].default}
+        />
+        <BedgeName type={bedges[index].type}>
+          {bedges[index].type === "active" ? bedges[index]?.text : "?"}
+        </BedgeName>
+      </Bedge>
+    );
+  };
+
+  return (
+    <Container>
+      <Row>
+        {renderCell(0)}
+        {renderCell(1)}
+        {renderCell(2)}
+      </Row>
+      <Row>
+        {renderCell(3)}
+        {renderCell(4)}
+        {renderCell(5)}
+      </Row>
+      <Row>
+        {renderCell(6)}
+        {renderCell(7)}
+        {renderCell(8)}
+      </Row>
+    </Container>
+  );
 };
 
-export default Bedge;
+export default UserBedgeScreen;
 
 const Container = styled.View`
-  border: 1px solid blue;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Bedge = styled.View`
+  width: 33%;
+  height: 100px;
+  padding: 10px;
+  margin-bottom: 10px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BedgeImage = styled.Image`
+  width: 100%;
+  height: 100%;
+  margin-bottom: 5px;
+  object-fit: contain;
+`;
+
+const BedgeName = styled.Text<{ type: string }>`
+  font-size: 14px;
+  color: ${(props) => (props.type === "active" ? props.theme.textColor : props.theme.subTextColor)};
+`;
+
+const Row = styled.View`
+  flex-direction: row;
 `;
