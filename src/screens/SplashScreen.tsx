c7fbb793a99311f0cfb930f81dba6baf3ff8e014
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-06-06 14:54:11 +0900

    feat: Firebase 설치
    feat: Firebase 설치

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -2,6 +2,10 @@ import React, { useEffect } from "react";
 import styled from "styled-components/native";
 import { useNavigation } from "@react-navigation/native";
 
+// Firebase 되는지 테스트
+import { database } from "../../firebaseConfig";
+import { ref, set } from "firebase/database";
+
 import { RootNavigatorParamList } from "../navigations/RootNavigator";
 
 import Layout from "../components/Layout";
@@ -16,12 +20,27 @@ const SplashScreen = () => {
     }, 3000);
   }, [])
 
+  // Firebase 테스트용
+  const onPress = () => {
+    const date = new Date();
+    postTestData(Date.now(), date.toLocaleString());
+  };
+
+  const postTestData = (key:any, value:any) => {
+    set(ref(database, 'test/' + key.toString()), value);
+  };
+
   return (
     <>
       <Layout>
         <Text>
           하이
         </Text>
+        <Button onPress={onPress}>
+          <Text>
+            파이어 베이스
+          </Text>
+        </Button>
       </Layout>
     </>
   );
@@ -32,4 +51,9 @@ export default SplashScreen;
 // styled
 const Text = styled.Text`
   font-size: 20px;
+`;
+
+const Button = styled.TouchableOpacity`
+  width: 100%;
+  background-color: gold;
 `;
